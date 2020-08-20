353d402d7a1fdfd42980ea3646ca2f9c8ab4a9e9
OverDodo <unconfigured@null.spigotmc.org>
2020-08-20 21:13:16 +0200

    Some changes

--- src/main/java/com/dehys/norbecore/Main.java
+++ src/main/java/com/dehys/norbecore/Main.java
@@ -1,4 +1,23 @@
 package com.dehys.norbecore;
 
-public class Main {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    private static Main instance;
+
+    public static Main getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        instance = this;
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
 }
