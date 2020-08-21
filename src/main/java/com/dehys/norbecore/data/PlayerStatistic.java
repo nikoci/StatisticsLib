a52a2e167f13dae72a1f7a64c3269cb589564f9a
OverDodo <dodo.stenzi@gmx.de>
2020-08-21 23:03:45 +0200

    Added methods for Blocks Broken to PlayerStatistic.java

--- src/main/java/com/dehys/norbecore/data/PlayerStatistic.java
+++ src/main/java/com/dehys/norbecore/data/PlayerStatistic.java
@@ -1,15 +1,20 @@
 package com.dehys.norbecore.data;
 
+import javafx.scene.paint.Material;
+
+import java.util.HashMap;
 import java.util.UUID;
 
 public class PlayerStatistic {
 
     private final UUID uuid;
     private final String playerID;
+    private HashMap<Material, Integer> blocksBroken;
 
     public PlayerStatistic(final UUID uuid, final String playerID) {
         this.uuid = uuid;
         this.playerID = playerID;
+        blocksBroken = new HashMap<>();
     }
 
     public static PlayerStatistic loadExisting(UUID uuid, String playerID) {
@@ -23,5 +28,17 @@ public class PlayerStatistic {
 
     public String getPlayerID() {
         return playerID;
+    }
+
+    public HashMap<Material, Integer> getBlocksBroken() {
+        return blocksBroken;
+    }
+
+    public Integer getBrokenBlocks(Material material) {
+        return blocksBroken.getOrDefault(material, 0);
+    }
+
+    public Integer getTotalBrokenBlocks() {
+        return blocksBroken.values().stream().mapToInt(Integer::intValue).sum();
     }
 }
