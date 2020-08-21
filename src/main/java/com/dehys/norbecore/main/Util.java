60b88e78d6d563f8caf3a47f5b50a66c2de1834c
OverDodo <unconfigured@null.spigotmc.org>
2020-08-21 16:08:50 +0200

    - UserData added - ConfigManager added - Util class added - Other

---
+++ src/main/java/com/dehys/norbecore/main/Util.java
@@ -0,0 +1,16 @@
+package com.dehys.norbecore.main;
+
+public class Util {
+
+    private static final String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+    public static String generatePlayerID(int length) {
+        StringBuilder builder = new StringBuilder();
+        while (length-- != 0) {
+            int character = (int) (Math.random() * ALPHA_NUMERIC_STRING.length());
+            builder.append(ALPHA_NUMERIC_STRING.charAt(character));
+        }
+        return builder.toString();
+    }
+
+}
