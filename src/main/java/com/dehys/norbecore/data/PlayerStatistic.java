36fe56ce3e459fd51265f988c3711dab2c9d0d24
OverDodo <dodo.stenzi@gmx.de>
2020-08-24 21:01:02 +0200

    Added documentation for PlayerStatistic.java

--- src/main/java/com/dehys/norbecore/data/PlayerStatistic.java
+++ src/main/java/com/dehys/norbecore/data/PlayerStatistic.java
@@ -51,123 +51,91 @@ public class PlayerStatistic {
         return userid;
     }
 
+    /**
+     * This method is meant to increase a {@link Statistic} with no (NONE) {@link Substatistic}
+     * @param statistic the statistic that wants to be increased
+     * @param amount the value by which the statistic should be increased
+     */
     public void addStatistic(@NotNull Statistic statistic, int amount) {
         addStatistic(statistic, null, null, amount);
     }
 
+    /**
+     * This method is meant to increase a {@link Statistic} with a MATERIAL {@link Substatistic}
+     * @param statistic the statistic that wants to be increased
+     * @param material define the {@link Material} of the statistic
+     * @param amount the value by which the statistic should be increased
+     */
     public void addStatistic(@NotNull Statistic statistic, Material material, int amount) {
-       addStatistic(statistic, material, null, amount);
+        addStatistic(statistic, material, null, amount);
     }
-    
+
+    /**
+     * This method is meant to increase a {@link Statistic} with a ENTITY {@link Substatistic}
+     * @param statistic the statistic that wants to be increased
+     * @param entityType define the {@link EntityType} of the statistic
+     * @param amount the value by which the statistic should be increased
+     */
     public void addStatistic(@NotNull Statistic statistic, EntityType entityType, int amount) {
         addStatistic(statistic, null, entityType, amount);
     }
 
     void addStatistic(@NotNull Statistic statistic, Material material, EntityType entityType, int amount) {
-       switch (statistic.getSubstatistic()) {
-           case MATERIAL:
-               assert material!=null;
-               addMaterialStatistic(statistic, material, amount);
-               break;
+        switch (statistic.getSubstatistic()) {
+            case MATERIAL:
+                assert material != null;
+                addMaterialStatistic(statistic, material, amount);
+                break;
 
-           case ENTITY:
-               assert entityType!=null;
-               
+            case ENTITY:
+                assert entityType != null;
 
-           case NONE:
-               addPlainStatistic(statistic, amount);
-       }
+
+            case NONE:
+                addPlainStatistic(statistic, amount);
+        }
     }
 
+    /**
+     * This method is meant for {@link Statistic} with no (NONE) {@link Substatistic}
+     * @param statistic the statistic that wants to be retrieved
+     * @return returns the statistic selected
+     * If a Statistic with Substatistics has been selected,
+     * this method will return the sum of all the Substatistics of that Statistic
+     */
     public int getStatistic(@NotNull Statistic statistic) {
         return getStatistic(statistic, null, null);
     }
 
+    /**
+     * This method is meant for {@link Statistic} with a MATERIAL {@link Substatistic}
+     * @param statistic the statistic that wants to be retrieved
+     * @param material define the {@link Material} of the statistic
+     * @return returns the statistic specified by the {@link Material}
+     * Will return the sum of all the Substatistics of that Statistic if material = null
+     */
     public int getStatistic(@NotNull Statistic statistic, Material material) {
         return getStatistic(statistic, material, null);
     }
 
+    /**
+     * This method is meant for {@link Statistic} with a ENTITY {@link Substatistic}
+     * @param statistic the statistic that wants to be retrieved
+     * @param entityType define the {@link EntityType} of the statistic
+     * @return returns the statistic specified by the {@link EntityType}
+     * Will return the sum of all the Substatistics of that Statistic if entityType = null
+     */
     public int getStatistic(@NotNull Statistic statistic, EntityType entityType) {
         return getStatistic(statistic, null, entityType);
     }
 
-    private int getStatistic(@NotNull Statistic statistic, Material material, EntityType entityType) {
-        switch (statistic.getSubstatistic()) {
-            case MATERIAL:
-                assert material!=null;
-                return getMaterialStatistic(statistic).isPresent() ? getMaterialStatistic(statistic).get().getOrDefault(material, 0) : 0;
-            case ENTITY:
-                assert entityType!=null;
-                return getEntityStatistic(statistic).isPresent() ? getEntityStatistic(statistic).get().getOrDefault(entityType, 0) : 0;
-            case NONE:
-                return getPlainStatistic(statistic);
-
-            default:
-                return 0;
-        }
-    }
-
-
-
-
-    private Integer getPlainStatistic(@NotNull Statistic statistic) {
-        return plainStatistics.getOrDefault(statistic.getKey(), 0);
-    }
-
-    private Optional<HashMap<Material, Integer>> getMaterialStatistic(Statistic statistic) {
-        if(statistic.getSubstatistic()!=Substatistic.MATERIAL) return Optional.empty();
-        return Optional.ofNullable(materialStatistics.get(statistic.getKey()));
-    }
-
-    private Optional<HashMap<EntityType, Integer>> getEntityStatistic(Statistic statistic) {
-        if(statistic.getSubstatistic()!=Substatistic.ENTITY) return Optional.empty();
-        return Optional.ofNullable(entityStatistics.get(statistic.getKey()));
-    }
-
-    private void addPlainStatistic(Statistic statistic, int value) {
-        final String key = statistic.getKey();
-        if(statistic.hasSubstatistic()) {
-            throw new IllegalArgumentException("Given Statistic has substatistics");
-        }
-        if(!plainStatistics.containsKey(key)) {
-            plainStatistics.put(key, value);
-        } else {
-            plainStatistics.put(key, plainStatistics.get(key)+value);
-        }
-    }
-
-    private void addMaterialStatistic(Statistic statistic, Material material, int value) {
-        final String key = statistic.getKey();
-        if(statistic.getSubstatistic() != Substatistic.MATERIAL) {
-            throw new IllegalArgumentException("Given Statistic has no material substatistics");
-        }
-        HashMap<Material, Integer> materialMap;
-        if(!materialStatistics.containsKey(key)) {
-            materialMap = new HashMap<>();
-            materialMap.put(material, value);
-            materialStatistics.put(key, materialMap);
-        } else {
-            materialMap = materialStatistics.get(key);
-            materialMap.put(material, materialMap.get(material)+value);
-        }
-    }
-
-    private void addEntityStatistic(Statistic statistic, EntityType entityType, int value) {
-        final String key = statistic.getKey();
-        if(statistic.getSubstatistic() != Substatistic.ENTITY) {
-            throw new IllegalArgumentException("Given Statistic has no entity substatistics");
-        }
-        HashMap<EntityType, Integer> entityMap;
-        if(!entityStatistics.containsKey(key)) {
-            entityMap = new HashMap<>();
-            entityMap.put(entityType, value);
-            entityStatistics.put(key, entityMap);
-        } else {
-            entityMap = entityStatistics.get(key);
-            entityMap.put(entityType, entityMap.get(entityType)+value);
-        }
-    }
-    
+    /**
+     * This method will save all of the {@link Statistic}
+     * that have an UNDEFINED {@link Substatistic} to the MySQL-Database
+     * Running this method directly will cause it to run sync
+     * If you want to run this method async, use {@link StatisticsManager#saveStatistics()}
+     * or run the method in another {@link Thread}
+     */
     public void savePlainStatistics() {
         try {
             PreparedStatement plainStatement = SQL.prepareStatement("INSERT INTO plainstatistics (userid, statistic, amount) VALUES (?, ?, ?)");
@@ -178,13 +146,20 @@ public class PlayerStatistic {
                 plainStatement.setInt(3, entry.getValue());
                 plainStatement.executeUpdate();
             }
-            
+
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
 
     }
 
+    /**
+     * This method will save all of the {@link Statistic}
+     * that have a MATERIAL {@link Substatistic} to the MySQL-Database
+     * Running this method directly will cause it to run sync
+     * If you want to run this method async, use {@link StatisticsManager#saveStatistics()}
+     * or run the method in another {@link Thread}
+     */
     public void saveMaterialStatistics() {
         try {
             PreparedStatement materialStatement = SQL.prepareStatement("INSERT INTO materialstatistics (userid, statistic, material, amount) VALUES (?, ?, ?, ?)");
@@ -205,6 +180,13 @@ public class PlayerStatistic {
 
     }
 
+    /**
+     * This method will save all of the {@link Statistic}
+     * that have an UNDEFINED {@link Substatistic} to the MySQL-Database
+     * Running this method directly will cause it to run sync
+     * If you want to run this method async, use {@link StatisticsManager#saveStatistics()}
+     * or run the method in another {@link Thread}
+     */
     public void saveEntityStatistics() {
         try {
             PreparedStatement entityStatement = SQL.prepareStatement("INSERT INTO plainstatistics (userid, statistic, entity, amount) VALUES (?, ?, ?, ?)");
@@ -220,6 +202,87 @@ public class PlayerStatistic {
             }
         } catch (SQLException throwables) {
             throwables.printStackTrace();
+        }
+    }
+
+    private int getStatistic(@NotNull Statistic statistic, Material material, EntityType entityType) {
+        switch (statistic.getSubstatistic()) {
+
+            case MATERIAL:
+                if (material != null)
+                    return getMaterialStatistic(statistic).isPresent() ? getMaterialStatistic(statistic).get().getOrDefault(material, 0) : 0;
+                else
+                    return getMaterialStatistic(statistic).isPresent() ? getMaterialStatistic(statistic).get().values().stream().mapToInt(Integer::intValue).sum() : 0;
+
+            case ENTITY:
+                if (entityType != null)
+                    return getEntityStatistic(statistic).isPresent() ? getEntityStatistic(statistic).get().getOrDefault(entityType, 0) : 0;
+                else
+                    return getEntityStatistic(statistic).isPresent() ? getEntityStatistic(statistic).get().values().stream().mapToInt(Integer::intValue).sum() : 0;
+            case NONE:
+                return getPlainStatistic(statistic);
+
+            default:
+                return 0;
+        }
+    }
+
+
+    private Integer getPlainStatistic(@NotNull Statistic statistic) {
+        return plainStatistics.getOrDefault(statistic.getKey(), 0);
+    }
+
+    private Optional<HashMap<Material, Integer>> getMaterialStatistic(Statistic statistic) {
+        if (statistic.getSubstatistic() != Substatistic.MATERIAL) return Optional.empty();
+        return Optional.ofNullable(materialStatistics.get(statistic.getKey()));
+    }
+
+    private Optional<HashMap<EntityType, Integer>> getEntityStatistic(Statistic statistic) {
+        if (statistic.getSubstatistic() != Substatistic.ENTITY) return Optional.empty();
+        return Optional.ofNullable(entityStatistics.get(statistic.getKey()));
+    }
+
+    private void addPlainStatistic(Statistic statistic, int value) {
+        final String key = statistic.getKey();
+        if (statistic.hasSubstatistic()) {
+            throw new IllegalArgumentException("Given Statistic has substatistics");
+        }
+        if (!plainStatistics.containsKey(key)) {
+            plainStatistics.put(key, value);
+        } else {
+            plainStatistics.put(key, plainStatistics.get(key) + value);
+        }
+    }
+
+    private void addMaterialStatistic(Statistic statistic, Material material, int value) {
+        final String key = statistic.getKey();
+        if (statistic.getSubstatistic() != Substatistic.MATERIAL) {
+            throw new IllegalArgumentException("Given Statistic has no material substatistics");
+        }
+        HashMap<Material, Integer> materialMap;
+        if (!materialStatistics.containsKey(key)) {
+            materialMap = new HashMap<>();
+            materialMap.put(material, value);
+            materialStatistics.put(key, materialMap);
+        } else {
+            materialMap = materialStatistics.get(key);
+            materialMap.put(material, materialMap.get(material) + value);
+        }
+    }
+
+    private void addEntityStatistic(Statistic statistic, EntityType entityType, int value) {
+        final String key = statistic.getKey();
+        if (statistic.getSubstatistic() != Substatistic.ENTITY) {
+            throw new IllegalArgumentException("Given Statistic has no entity substatistics");
+        }
+        HashMap<EntityType, Integer> entityMap;
+        if (!entityStatistics.containsKey(key)) {
+            entityMap = new HashMap<>();
+            entityMap.put(entityType, value);
+            entityStatistics.put(key, entityMap);
+        } else {
+            entityMap = entityStatistics.get(key);
+            entityMap.put(entityType, entityMap.get(entityType) + value);
         }
     }
 }
