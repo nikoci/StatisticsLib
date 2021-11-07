0c1c025da15602d725f4ff8067dcf3fd33b188a2
OverDodo <dodo.stenzi@gmx.de>
2021-11-07 13:18:34 +0100

    Made use of new record feature

--- src/main/java/com/devflask/statisticslib/lib/commands/ReloadCommand.java
+++ src/main/java/com/devflask/statisticslib/lib/commands/ReloadCommand.java
@@ -1,18 +1,12 @@
 package com.devflask.statisticslib.lib.commands;
 
+import com.devflask.statisticslib.lib.main.PlayerStatistics;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import com.devflask.statisticslib.lib.main.PlayerStatistics;
 import org.jetbrains.annotations.NotNull;
 
-public class ReloadCommand implements CommandExecutor {
-
-    private final PlayerStatistics plugin;
-
-    public ReloadCommand(PlayerStatistics plugin) {
-        this.plugin = plugin;
-    }
+public record ReloadCommand(PlayerStatistics plugin) implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
